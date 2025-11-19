e0e5429de163c229ab2010dc26003554f7122d26
rotarymars <s.goto2050@gmail.com>
2025-11-19 15:14:42 +0900

    creating Message and uart_io class

--- modules/robot.py
+++ modules/robot.py
@@ -1,10 +1,16 @@
 import modules.constants as consts
 import modules.logger as logger
 
+class Message:
+  pass
+
+class uart_io:
+  pass
+
 class Robot:
   def __init__(self):
     self.logger = logger.get_logger()
-    
+
 
 if __name__ == "__main__":
   pass
