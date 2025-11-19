8237c2b7aca5bccfd80638a22d15bc3b7449884f
rotarymars <s.goto2050@gmail.com>
2025-11-19 15:12:40 +0900

    initializing robot class with its logger

--- modules/robot.py
+++ modules/robot.py
@@ -1,2 +1,10 @@
+import modules.constants as consts
+import modules.logger as logger
+
+class Robot:
+  def __init__(self):
+    self.logger = logger.get_logger()
+    
+
 if __name__ == "__main__":
   pass
