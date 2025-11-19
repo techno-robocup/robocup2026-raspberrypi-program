513f4a6b4cbf9aefcf90ad7bd553c57e87dbe441
K10-K10 <keito.tottori@gmail.com>
2025-11-20 08:30:15 +0900

    Add:linetrace cam pre call back

--- modules/camera.py
+++ modules/camera.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+import cv2
 import threading
 import modules.logger
 from typing import Callable, Any, Dict, Tuple
@@ -7,6 +8,7 @@ from picamera2 import Picamera2, CompletedRequest
 
 logger = modules.logger.get_logger()
 
+BLACK_WHITE_THRESHOLD = 55
 
 class Camera:
   """Camera class for handling Picamera2 operations."""
@@ -54,4 +56,44 @@ def Rescue_precallback_func(request: CompletedRequest) -> None:
   modules.logger.get_logger().info("Rescue Camera pre-callback triggered")
   with MappedArray(request, "lores") as mapped_array:
     image = mapped_array.array
+    current_time = time.time()
+    cv2.imwrite(f"bin/{current_time:.3f}_rescue_origin.jpg")
     modules.robot.robot.write_rescue_image(image)
+
+
+def apply_center_vignette(img, strength=0.5):#filter function
+  h, w = img.shape[:2]
+  kernel_x = cv2.getGaussianKernel(w, w * strength)
+  kernel_y = cv2.getGaussianKernel(h, h * strength)
+  mask = kernel_y @ kernel_x.T
+  mask = mask / mask.max()
+  vignette = np.zeros_like(img)
+  for i in range(3):  # RGB
+      vignette[:,:,i] = img[:,:,i] * mask
+  return vignette
+
+def Linetrace_Camera_Pre_callback(request):
+  logger.debug("Linetrace Camera Pre call-back called")
+  current_time = time.time()
+  try:
+    with MappedArray(request, "lores") as m:
+      image = m.array
+      h,w = image.shape[:2]
+      crop_w = int(w * 0.9)
+      x_start = (w - crop_w) // 2
+      image = image[:, x_start:x_start + crop_w]
+      image = cv2.resize(image,(w,h), interpolation=cv2.INTER_LINEAR)
+      cv2.imwrite(f"bin/{current_time:.3f}_linetrace_origin.jpg", image)
+      image = apply_center_vignette(image)
+      cv2.imwrite(f"bin/{current_time:.3f}_linetrace_format.jpg",image)
+      gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+      _, binary_image = cv2.threshold(gray_image, BLACK_WHITE_THRESHOLD, 255, cv2.THRESH_BINARY_INV)
+
+      cv2.imwrite(f"bin/{current_time:.3f}_binary.jpg")
+      
+
+  except SystemExit:
+    print("SystemExit caught")
+    raise
+  except Exception as e:
+    logger.error(f"Error in line tracing: {e}")
