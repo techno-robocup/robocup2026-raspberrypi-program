b019979eaca75740ce31562b08aa0a71bb245823
rotarymars <s.goto2050@gmail.com>
2025-12-10 16:32:30 +0900

    rotating iamge in linetrace

--- modules/camera.py
+++ modules/camera.py
@@ -430,6 +430,7 @@ def Linetrace_Camera_Pre_callback(request):
   try:
     with MappedArray(request, "lores") as m:
       image = m.array
+      image = cv2.rotate(image, cv2.ROTATE_180)
       h, w = image.shape[:2]
       crop_w = int(w * 0.85)
       x_start = (w - crop_w) // 2
