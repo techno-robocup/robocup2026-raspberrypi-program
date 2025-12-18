157c581c161fd4b7740e0b60e6652d57b13a6e15
K10-K10 <keito.tottori@gmail.com>
2025-12-18 15:24:20 +0900

    fix:constants value

--- modules/constants.py
+++ modules/constants.py
@@ -73,8 +73,8 @@ MIN_BLACK_LINE_AREA = 300
 MIN_GREEN_AREA = 200
 MIN_RED_AREA = 200
 
-TURN_180_TIME = 12.0
-TURN_30_TIME = 3.0
+TURN_180_TIME = 6.0
+TURN_30_TIME = 1.0
 
 # Green mark intersection turning times (seconds)
 GREEN_MARK_APPROACH_TIME = 0.2
