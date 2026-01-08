6afe31ed434d835a70bd88d9f87d40019a328bfc
rotarymars <s.goto2050@gmail.com>
2026-01-08 10:21:44 +0900

    Add rescue turning angle reset in release_ball function for improved control

--- main.py
+++ main.py
@@ -670,6 +670,7 @@ def release_ball() -> bool:
   robot.set_arm(3072, 0)
   robot.send_arm()
   sleep_sec(0.5)
+  robot.write_rescue_turning_angle(0)
   robot.set_speed(1400, 1400)
   sleep_sec(1)
   robot.set_speed(1750, 1250)
