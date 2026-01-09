27f2f659d13f30df7f7fe22c7f29fe0ee5f73469
K10-K10 <keito.tottori@gmail.com>
2026-01-09 15:30:28 +0900

    fix: arm value

--- main.py
+++ main.py
@@ -648,7 +648,7 @@ def catch_ball() -> int:
   sleep_sec(1.5)
   robot.set_speed(1500, 1500)
   robot.send_speed()
-  robot.set_arm(1380, 0)
+  robot.set_arm(1420, 0)
   robot.send_arm()
   robot.set_speed(1650, 1650)
   sleep_sec(2.2)
