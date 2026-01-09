936f018b9ca7a1dc771a329e2e74a152d5693d28
rotarymars <s.goto2050@gmail.com>
2026-01-09 14:16:17 +0900

    fix: adjust robot speed during object avoidance
    - Increased the speed from 1550 to 1580 for improved performance
    - Ensures smoother navigation while avoiding obstacles

--- main.py
+++ main.py
@@ -1007,7 +1007,7 @@ if __name__ == "__main__":
           while robot.linetrace_slope is None or robot.line_area <= consts.MIN_OBJECT_AVOIDANCE_LINE_AREA:
             logger.info("Turning around in object avoidance...")
             robot.write_last_slope_get_time(time.time())
-            robot.set_speed(1550, 1800)
+            robot.set_speed(1580, 1800)
             robot.send_speed()
             robot.update_button_stat()
             if robot.robot_stop:
