5fae158e26566f2e6f5e25f54dbe546ceadfe04d
rotarymars <s.goto2050@gmail.com>
2026-01-10 15:57:57 +0900

    Revert "fix: add rescue turning angle to robot control"
    This reverts commit ba4b68f1f2c92826932b3f73d6787a68c244c462.

--- main.py
+++ main.py
@@ -1028,7 +1028,6 @@ if __name__ == "__main__":
         ]:
           set_target()
       else:
-        robot.write_rescue_turning_angle(740)
         if True:
           ultrasonic_info = robot.ultrasonic
           if not exit_cage_flag:
