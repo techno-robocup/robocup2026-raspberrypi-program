be10bc263762e64feda3368db30e6395c6d9ba33
rotarymars <s.goto2050@gmail.com>
2026-01-22 17:11:37 +0900

    fix: prepend 'uv run' to formatting commands in format.sh

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
-isort .
-yapf --style .style.yapf --in-place --recursive . --exclude .direnv/ --exclude .venv
-flake8 --exclude .venv --ignore E111,E114,E501,E126,W504,E125
+uv run isort .
+uv run yapf --style .style.yapf --in-place --recursive . --exclude .direnv/ --exclude .venv
+uv run flake8 --exclude .venv --ignore E111,E114,E501,E126,W504,E125
