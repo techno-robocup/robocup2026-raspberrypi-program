c470d35a7dc142f6d32c2b2edf8954d23bd04a0f
K10-K10 <keito.tottori@gmail.com>
2026-01-29 16:39:48 +0900

    refactor: caluclate ball func

--- main.py
+++ main.py
@@ -912,14 +912,17 @@ def calculate_ball() -> tuple[int, int]:
   if robot.ball_catch_offset_flag and robot.ball_catch_dist_flag and robot.ball_near_flag:
     diff_angle = 0
     dist_term = -80
+  if robot.ball_catch_offset_flag and robot.ball_catch_dist_flag and (not robot.ball_near_flag):
+    diff_angle = 0
+    dist_term = 0
+  if robot.ball_catch_offset_flag and (not robot.ball_catch_dist_flag) and (not robot.ball_near_flag):
+    dist_term *= 1.0
   if (not robot.ball_catch_offset_flag) and robot.ball_catch_dist_flag and robot.ball_near_flag:
     diff_angle *= -0.5
     dist_term = -80
   if (not robot.ball_catch_offset_flag) and robot.ball_catch_dist_flag and (not robot.ball_near_flag):  # offset
    # diff_angle *= 1.3
     dist_term = 0
-  if robot.ball_catch_offset_flag and (not robot.ball_catch_dist_flag) and (not robot.ball_near_flag):
-    dist_term *= 1.5
   base_L = 1500 + diff_angle + dist_term
   base_R = 1500 - diff_angle + dist_term
   base_L = int(base_L)
