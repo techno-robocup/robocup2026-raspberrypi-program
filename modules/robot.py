2815145d744865079104228127415feba5a6b413
K10-K10 <keito.tottori@gmail.com>
2026-02-07 17:49:36 +0900

    add new member variable

--- modules/robot.py
+++ modules/robot.py
@@ -240,6 +240,7 @@ class Robot:
     self.__rescue_y: Optional[float] = None
     self.__rescue_target: int = consts.TargetList.SILVER_BALL.value
     self.__rescue_turning_angle: int = 0  # Total revolutions
+    self.__rescue_saved_time: int = 0
     self.__ball_catch_dist_flag = False  # catch ball flag
     self.__ball_catch_offset_flag = False
     self.__ball_near_flag = False
@@ -357,6 +358,16 @@ class Robot:
     """
     assert self.__uart_device is not None
     return self.__uart_device.send("GET button") == "ON"
+
+  def write_rescue_saved_time(self, time: int) -> None:
+    with self.__rescue_camera_lock:
+      self.__rescue_saved_time
+    return None
+
+  @property
+  def rescue_saved_time(self) -> int:
+    with self.__rescue_camera_lock:
+      return self.__rescue_saved_time
 
   def write_rescue_image(self, image: npt.NDArray[np.uint8]) -> None:
     """Store rescue camera image for YOLO processing (thread-safe).
