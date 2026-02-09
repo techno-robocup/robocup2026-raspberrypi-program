7f4f4e7bef26b26f123f48fbbf383f390ada1595
rotarymars <s.goto2050@gmail.com>
2026-02-09 18:17:09 +0900

    adding W503 to ignore in lint

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 uv run isort .
 uv run yapf --style .style.yapf --in-place --recursive . --exclude .direnv/ --exclude .venv
-uv run flake8 --exclude .venv --ignore E111,E114,E501,E126,W504,E125
+uv run flake8 --exclude .venv --ignore E111,E114,E501,E126,W504,E125,W503
