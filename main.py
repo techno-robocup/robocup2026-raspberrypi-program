2146e0ba6d92e07f46fa3990a137e22d58cb8b16
K10-K10 <keito.tottori@gmail.com>
2026-02-19 16:19:48 +0900

    modified: back speed before searching target

--- main.py
+++ main.py
@@ -1173,7 +1173,7 @@ def handle_before_search() -> None:
   if hasFoundExit == -1:
     robot.set_speed(1500, 1500)
     robot.send_speed()
-    robot.set_speed(1450, 1450)
+    robot.set_speed(1350, 1350)
     sleep_sec(4.0)
     robot.set_speed(1750, 1250)
     sleep_sec(consts.TURN_90_TIME)
