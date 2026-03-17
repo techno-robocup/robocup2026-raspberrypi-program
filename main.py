cf07661c9ce3e7b1868ccc8cff15d55db3556fde
K10-K10 <keito.tottori@gmail.com>
2026-03-18 06:08:02 +0900

    fixed: del width flag

--- main.py
+++ main.py
@@ -410,9 +410,7 @@ def should_execute_line_recovery(arg_line_area: Optional[float]) -> bool:
   line_is_wide = robot.line_width > consts.LINETRACE_CAMERA_LORES_WIDTH / 15
 
   # Only trigger when area is small AND x-offset is significant or line is wide (indicating a gap or veering off)
-  should_recover = is_line_at_the_bottom and (
-      (area_condition and x_offset_condition) or
-      (area_condition and line_is_wide))
+  should_recover = is_line_at_the_bottom and (area_condition and x_offset_condition)
 
   is_vertical = abs(math.degrees(robot.line_skeleton_angle) + 90) < 5
 
